ff8df28bf775f2890a27301f4b4223596243f4af
takahiyo <141232864+takahiyo@users.noreply.github.com>
2026-01-26 13:47:45 +0900

    Update PanelCanvas.tsx

--- src/presentation/components/PanelCanvas.tsx
+++ src/presentation/components/PanelCanvas.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Layer, Rect, Stage } from 'react-konva';
 import type { PanelTemplate } from '../../domain/types/panelTemplate';
 
